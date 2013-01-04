25d8e25bf161cc0ff6c18078d81ad6e8fe1883c7
dubreu_j <dubreu_j@epitech.eu>
2013-01-04 01:14:58 +0100

    Add basic create char window

--- Assets/Sources/LoginScreen/PanelLogin.cs
+++ Assets/Sources/LoginScreen/PanelLogin.cs
@@ -13,12 +13,14 @@ public class 				PanelLogin : MonoBehaviour
 	{
 		LOGIN,
 		REGISTER,
-		CHARA
+		CHARA,
+		CREATE
 	}
 	
 	public GameObject		_panelLogin;
 	public GameObject		_panelRegister;
 	public GameObject		_panelChar;
+	public GameObject		_panelCreate;
 	
 	private ePanelLoginState _state = ePanelLoginState.LOGIN;
 	private static PanelLogin _instance = null;
@@ -46,19 +48,22 @@ public class 				PanelLogin : MonoBehaviour
 		switch (_state)
 		{
 		case ePanelLoginState.LOGIN:
-			activePanels(true, false, false); break;
+			activePanels(true, false, false, false); break;
 		case ePanelLoginState.REGISTER:
-			activePanels(false, true, false); break;
+			activePanels(false, true, false, false); break;
 		case ePanelLoginState.CHARA:
-			activePanels(false, false, true); break;
+			activePanels(false, false, true, false); break;
+		case ePanelLoginState.CREATE:
+			activePanels(false, false, false, true); break;
 		}
 	}
 	
-	private void			activePanels(bool login, bool register, bool chara)
+	private void			activePanels(bool login, bool register, bool chara, bool create)
 	{
 		_panelLogin.SetActive(login);
 		_panelRegister.SetActive(register);
 		_panelChar.SetActive(chara);
+		_panelCreate.SetActive(create);
 		if (chara)
 			_panelChar.GetComponent<DisplayPersoList>().loadCharacters();
 	}
