95e52d08d509ec6f143e74325f371e0452700dd5
dubreu_j <dubreu_j@epitech.eu>
2013-06-15 17:28:11 +0200

    Prepare to add 3D animated assets

---
+++ Assets/Sources/Game/GamePlay/AnimationStateManager.cs
@@ -0,0 +1,19 @@
+/*
+ * 
+ * 
+ * 
+ */
+
+using						UnityEngine;
+using						System.Collections;
+
+public class 				AnimationStateManager : MonoBehaviour
+{
+	void					Start()
+	{
+	}
+	
+	void					Update()
+	{
+	}
+}
