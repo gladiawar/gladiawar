205af1d52801a1d10de6d34c7c4aac6562b0bba3
dubreu_j <jean-baptiste.dubreucq@epitech.eu>
2013-07-06 19:30:03 +0200

    Multi player visible, network works for player move

--- Assets/Sources/Game/GamePlay/LogicInGame.cs
+++ Assets/Sources/Game/GamePlay/LogicInGame.cs
@@ -10,11 +10,27 @@ using						System.Collections;
 
 public class 				LogicInGame : MonoBehaviour
 {
+	private bool			_wait = true;
+	
 	void					Start()
+	{
+	}
+	
+	void					Update()
+	{
+		if (_wait && PhotonNetwork.room.playerCount > 1)
+		{
+			Invoke("SpawnPlayer", 3.0f);
+			_wait = false;
+		}
+	}
+	
+	void					SpawnPlayer()
 	{
 		GameObject			myPlayer = PhotonNetwork.Instantiate("NormalPlayer", new Vector3(11, 0.65f, -25), Quaternion.Euler(new Vector3(0, 0, 0)), 0);
 		PhotonView			pv;
 		
+		Debug.Log("spawn");
 		pv = myPlayer.GetComponent<PhotonView>();
 		pv.ownerId = PhotonNetwork.player.ID;
 	}
