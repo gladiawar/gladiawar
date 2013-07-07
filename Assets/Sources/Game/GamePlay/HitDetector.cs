fc179be8372942d2dc7b988f9704d1fba84510ba
dubreu_j <jean-baptiste.dubreucq@epitech.eu>
2013-07-07 16:43:31 +0200

    new detection test

--- Assets/Sources/Game/GamePlay/HitDetector.cs
+++ Assets/Sources/Game/GamePlay/HitDetector.cs
@@ -9,10 +9,12 @@ using						System.Collections;
 
 public class 				HitDetector : MonoBehaviour
 {
+	public GameObject		_me;
+	
 	void					OnTriggerEnter(Collider other)
 	{
 		Debug.Log(other.tag);
-		if (other.tag.Equals("Player") && other.gameObject != gameObject)
+		if (other.tag.Equals("Player") && other.gameObject != _me)
 			Debug.Log("touched");
 	}
 }
