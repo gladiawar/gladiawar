f03b6639a8af8ef640be5f0fb7be28e301fde7df
Claude Ramseyer <claude.ramseyer@epitech.eu>
2013-08-10 18:47:46 +0200

    Tabs/Options prefab WIP

--- Assets/Sources/Components/GTabButton/GTabButton.cs
+++ Assets/Sources/Components/GTabButton/GTabButton.cs
@@ -5,10 +5,14 @@ public class GTabButton : GButton_OnClick {
 	
 	public GTabs_Content content;
 	
-	public GTabs tabs { get; set; }
+	public GTabs tabs {get; set;}
+	
+	public void Start() {
+		this.content.gameObject.SetActive(false);	
+	}
 	
 	public override void OnClick(GButton caller) {
-		this.tabs.active(this);
+		this.tabs.OnActive(this);
 	}
 	
 }
--- Assets/Sources/Components/GTabs/GTabs_Container.cs
+++ Assets/Sources/Components/GTabs/GTabs_Container.cs
@@ -2,14 +2,22 @@ using UnityEngine;
 using System.Collections;
 
 public class GTabs_Container : GladiawarBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
+	protected GTabs_Content content;
+	
+	public void OnActive(GTabs_Content content) {
+		this.content = content;
+		this.show();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	protected void show() {
+		Transform contentT = this.content.transform;
+		Transform thisT = this.transform;
+		
+		contentT.localPosition = new Vector3(0f, 0f, 0f);
+		contentT.localRotation = thisT.localRotation;
+		contentT.localScale = thisT.localScale;
+		contentT.gameObject.SetActive(true);
 	}
+	
 }
