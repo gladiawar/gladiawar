94daaf26a1c6a00004f4b7fce7bd8e698755f24d
Claude Ramseyer <claude.ramseyer@epitech.eu>
2013-08-25 17:25:29 +0200

    Add GInput prefab

---
+++ Assets/Sources/Components/GInput/GInput.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * 
+ * 
+ * 
+ * @author Claude Ramseyer
+ */
+public class GInput : MonoBehaviour {
+	
+	//Constant
+	public const float LABEL_LENGTH = 50f;
+	public const float INPUT_LENGTH = 100f;
+	public const float MARGIN = 10f;
+	public const float BORDER = 7f;
+	
+	
+	//Editor attributes
+	public UILabel label;
+	public UIInput input;
+	
+	//Properties
+	public string LabelText {
+		get {
+			return this.label.text;
+		}
+		set {
+			this.label.text = value;
+		}
+	}
+	public string InputText {
+		get {
+			return this.input.text;
+		}
+		set {
+			this.input.text = value;
+		}
+	}
+	public string InputDefault {
+		get {
+			return this.input.defaultText;
+		}
+		set {
+			this.input.defaultText = value;
+		}
+	}
+	
+	//Functions
+	public void setInputLength(float length = GInput.INPUT_LENGTH) {
+		//Set line width with taking BORDER length
+		var label = this.input.label;
+		var corrected = length - (GInput.BORDER * 2);
+		label.lineWidth = (int) corrected;
+		
+		//Set background scale
+		var background = this.input.GetComponentInChildren<UISprite>();
+		var scale = background.transform.localScale;
+		background.transform.localScale = new Vector3(length, scale.y, scale.z);
+	}
+	
+	public void setLabelLength(float length = GInput.LABEL_LENGTH) {
+		//Set label length
+		var lpos = this.label.transform.localPosition;
+		this.label.transform.localPosition = new Vector3(length, lpos.y, lpos.z);
+		
+		//Move input
+		var ipos = this.input.transform.localPosition;
+		this.input.transform.localPosition = new Vector3(length + GInput.MARGIN, ipos.y, ipos.z);
+	}
+	
+}
