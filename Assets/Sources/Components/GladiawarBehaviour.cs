d9b3d975387b9eae7367e81540e9276e89bb8ea9
Claude Ramseyer <claude.ramseyer@epitech.eu>
2013-09-07 00:30:01 +0200

    Implement getPlayers/getCurrentPlayer in GladiawarBehavior

--- Assets/Sources/Components/GladiawarBehaviour.cs
+++ Assets/Sources/Components/GladiawarBehaviour.cs
@@ -1,9 +1,6 @@
 using UnityEngine;
 using System.Collections;
 
-<<<<<<< HEAD
-public class GladiawarBehaviour : MonoBehaviour {
-=======
 /**
  * This class provide extensions methods to MonoBehaviour
  * 
@@ -18,6 +15,5 @@ public static class GladiawarBehaviour {
 	public static List<GladiatorNetwork> getPlayers(this MonoBehaviour obj) {
 		return LogicInGame.Instance.PlayerList;
 	}
->>>>>>> 3466a78... Implement getPlayers/getCurrentPlayer in GladiawarBehavior
 	
 }
