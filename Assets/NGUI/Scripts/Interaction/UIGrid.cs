5a5de00b51750bd391cce01eb55df7585ecce86e
Julien Delaigues <julien.delaigues@gmail.com>
2013-09-07 16:16:00 +0200

    Working scoreboard need value update

--- Assets/NGUI/Scripts/Interaction/UIGrid.cs
+++ Assets/NGUI/Scripts/Interaction/UIGrid.cs
@@ -29,7 +29,7 @@ public class UIGrid : MonoBehaviour
 	public bool sorted = false;
 	public bool hideInactive = true;
 	public GameObject scoreboardLinePrefab;
-	private Dictionary<GameObject, GladiatorNetwork> scoreboardEntries = new Dictionary<GameObject, GladiatorNetwork>();
+	private Dictionary<GameObject, GladiatorNetwork> scoreboardEntries = new Dictionary<GameObject, GladiatorNetwork> ();
 	bool mStarted = false;
 
 	void Start ()
@@ -48,25 +48,31 @@ public class UIGrid : MonoBehaviour
 
 	void Update ()
 	{
-		foreach (var gn in this.getPlayers()) {
-			if (!scoreboardEntries.ContainsValue(gn))
-			{
-				GameObject obj = (GameObject)Instantiate (scoreboardLinePrefab);
-				obj.transform.parent = this.transform;
-				obj.transform.localScale = new Vector3(1f,1f,1f);
-				obj.transform.localPosition = new Vector3(0f,0f,0f);
+		if (Input.GetKey (KeyCode.Tab)) {
+			for (int i = 0; i < this.transform.childCount; ++i)
+				this.transform.GetChild (i).gameObject.SetActive (true);
+			foreach (var gn in this.getPlayers()) {
+				if (!scoreboardEntries.ContainsValue (gn)) {
+					GameObject obj = (GameObject)Instantiate (scoreboardLinePrefab);
+					obj.transform.parent = this.transform;
+					obj.transform.localScale = new Vector3 (1f, 1f, 1f);
+					obj.transform.localPosition = new Vector3 (0f, 0f, 0f);
 				
-				scoreboardEntries.Add(obj,gn);
+					scoreboardEntries.Add (obj, gn);
 				
-				repositionNow = true;
+					repositionNow = true;
+				}
 			}
-		}
 
-		foreach(var entry in scoreboardEntries)
-		{
-			entry.Key.transform.FindChild("Name").GetComponent<UILabel>().text = entry.Value.name;
-			entry.Key.transform.FindChild("Kill").GetComponent<UILabel>().text = entry.Value.Kill.ToString();
-			entry.Key.transform.FindChild("Death").GetComponent<UILabel>().text = entry.Value.Death.ToString();
+			foreach (var entry in scoreboardEntries) {
+				entry.Key.transform.FindChild ("Name").GetComponent<UILabel> ().text = entry.Value.name;
+				entry.Key.transform.FindChild ("Kill").GetComponent<UILabel> ().text = entry.Value.Kill.ToString ();
+				entry.Key.transform.FindChild ("Death").GetComponent<UILabel> ().text = entry.Value.Death.ToString ();
+			}
+
+		} else {
+			for (int i = 0; i < this.transform.childCount; ++i)
+				this.transform.GetChild (i).gameObject.SetActive (false);
 		}
 		
 		if (repositionNow) {
